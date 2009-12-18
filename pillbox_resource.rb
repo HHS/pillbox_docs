0cadebfc811f3261b552063c4674928dff83cb50
Darius <jdarius@jdarius.local>
2009-12-18 04:09:17 -0500

    borrowed shape/color translation feature from the python for rht active_resource ruby wrapper

--- pillbox_resource.rb
+++ pillbox_resource.rb
@@ -1,3 +1,21 @@
+=begin
+
+USAGE
+
+name = 'aspirin'
+
+PillboxResource.api_key = "YOUR SECRET KEY"   
+pills = PillboxResource.find(:all, :params=>{"ingredient"=>name.strip.capitalize})
+if pills.empty?
+  puts "could not find #{name}"
+else
+  ...
+end
+
+=end
+
+
+
 begin
   require 'active_resource'
 rescue LoadError
@@ -25,8 +43,10 @@ unless Pillbox::ARES_VERSIONS.include?(ActiveResource::VERSION::STRING)
 end
 
 # Patch ActiveResource
+# handle a weird disclaimer message that is in XML
   module ActiveResource
     class Base           
+           
       	    def self.instantiate_collection(collection, prefix_options = {})
                 if collection.is_a?(Hash) && collection.size == 1
                   value = collection.values.first
@@ -36,8 +56,8 @@ end
                     [ instantiate_record(value, prefix_options) ]
                   end
                 else
-                  instantiate_record(collection.values.last.first, prefix_options)
-#                  collection.collect! { |record| instantiate_record(record, prefix_options) }
+                  (d,disclaimer), (p,collection) = collection.sort 
+                  collection.collect! { |record| instantiate_record(record, prefix_options) }
                 end
              end
     end
@@ -46,15 +66,82 @@ end
 
 
 
-
+require 'ruby-debug'
 
 class PillboxResource < ActiveResource::Base
   self.site = "http://pillbox.nlm.nih.gov/PHP/pillboxAPIService.php"
 
-#  def find(first, *args, options ={})
-#   # interpret options
-#   super
-#  end
+  SHAPES = {
+      'BULLET'=> 'C48335',
+      'CAPSULE'=> 'C48336',
+      'CLOVER'=> 'C48337',
+      'DIAMOND'=> 'C48338',
+      'DOUBLE_CIRCLE'=> 'C48339',
+      'FREEFORM'=> 'C48340',
+      'GEAR'=> 'C48341',
+      'HEPTAGON'=> 'C48342',
+      'HEXAGON'=> 'C48343',
+      'OCTAGON'=> 'C48344',
+      'OVAL'=> 'C48345',
+      'PENTAGON'=> 'C48346',
+      'RECTANGLE'=> 'C48347',
+      'ROUND'=> 'C48348',
+      'SEMI_CIRCLE'=> 'C48349',
+      'SQUARE'=> 'C48350',
+      'TEAR'=> 'C48351',
+      'TRAPEZOID'=> 'C48352',
+      'TRIANGLE'=> 'C48353'
+  }
+
+  COLORS = {
+      'BLACK'=> 'C48323',
+      'BLUE'=> 'C48333',
+      'BROWN'=> 'C48332',
+      'GRAY'=> 'C48324',
+      'GREEN'=> 'C48329',
+      'ORANGE'=> 'C48331',
+      'PINK'=> 'C48328',
+      'PURPLE'=> 'C48327',
+      'RED'=> 'C48326',
+      'TURQUOISE'=> 'C48334',
+      'WHITE'=> 'C48325',
+      'YELLOW'=> 'C48330'
+  }
+  
+
+  cattr_accessor :api_key
+
+  def find(first, options={})
+   super first, interpret_params(options)
+  end
+  def interpret_params(options = {})
+    opts = options['params'] || {}
+    opts['key'] ||= self.api_key
+    
+    begin
+      opts['color'] = case opts['color']
+      when NilClass; 
+      when /^[0-9A-Fa-f]+$/;           opts['color'] # valid hex     
+      else;                     COLORS[opts['color'].upcase]
+      end
+    rescue
+      # "color not found"
+    end
+
+    begin
+      opts['shape'] = case opts['shape']
+      when NilClass; 
+      when /^[0-9A-Fa-f]+$/;          opts['shape'] # valid hex
+      else;                     SHAPES[opts['shape'].upcase]
+      end
+    rescue
+      # "shape not found"
+    end
+
+    
+    opts.delete_if {|k,v| v.nil? }
+    options.merge!(opts)
+  end
 
   def shape; attributes['SPLSHAPE'] end 
   def color; attributes['SPLCOLOR'] end 
@@ -63,7 +150,7 @@ class PillboxResource < ActiveResource::Base
   def product_code; attributes['PRODUCT_CODE'] end
   def has_image?; attributes['HAS_IMAGE'] == '1' end
   def ingredients; attributes['INGREDIENTS'].split(";") end
-  def size; attributes['SPLSIZE'].to_i end
+  def size; attributes['SPLSIZE'].to_f end
   def image_id; attributes['image_id'] end
   def image_url; image_id ? "http://pillbox.nlm.nih.gov/assets/super_small/#{image_id}ss.png" : nil end
   def imprint; attributes['splimprint'] end
