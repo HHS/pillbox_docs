d288e86b08a452ab552437ec062d9c1a8142b2c7
kgautreaux <keith.gautreaux@gmail.com>
2010-10-18 12:11:43 -0500

    * Create RDoc documentation of the PillboxResource class. * Modify the README slightly to remove note on broken Ruby wrapper. * Add additional tests for some corner cases. * Improve the documentation of the PillboxResource library.

--- lib/pillbox_resource.rb
+++ lib/pillbox_resource.rb
@@ -1,10 +1,10 @@
 # -*- encoding: utf-8 -*-
-=begin
+=begin rdoc
 
 NOTE: This library utilizes version 2 of the Pillbox API, published 10/9/10.
 
 USAGE
-require 'pillbox_resource'
+require 'pillbox'
 
 name = 'aspirin'
 
@@ -18,7 +18,7 @@ end
 =end
 
 # Version check
-module Pillbox
+module Pillbox # :nodoc:
   ARES_VERSIONS = ['2.3.4', '2.3.5', '3.0.0']
 end
 
@@ -29,11 +29,12 @@ unless Pillbox::ARES_VERSIONS.include?(ActiveResource::VERSION::STRING)
   ERROR
 end
 
-# Patch ActiveResource
-# handle a weird disclaimer message that is in XML
+##
+# PillboxResource monkeypatches the ActiveResource::Base.instantiate_collection method
+# to remove the disclaimer included in all XML returned from the Pillbox API Service
 module ActiveResource
   class Base           
-     def self.instantiate_collection(collection, prefix_options = {})
+     def self.instantiate_collection(collection, prefix_options = {}) # :nodoc:
       if collection.is_a?(Hash) && collection.size == 1
         value = collection.values.first
         if value.is_a?(Array)
@@ -56,6 +57,10 @@ module ActiveResource
   end
 end
 
+##
+# PillboxResource is a subclass of ActiveResource::Base that provides additional convenience
+# methods and some parameter wrapping for querying the Pillbox API Service located at
+# http://pillbox.nlm.nih.gov/PHP/pillboxAPIService.php
 class PillboxResource < ActiveResource::Base
   self.site = "http://pillbox.nlm.nih.gov/PHP/pillboxAPIService.php"
 
@@ -81,7 +86,33 @@ class PillboxResource < ActiveResource::Base
       'TRIANGLE'=> 'C48353'
   }
   SHAPE_CODES = SHAPES.invert
-  def self.shapes; SHAPES.inject({}){|i,(k,v)| i.merge k.humanize => v } end
+  
+  ##
+  # Returns a hash of the accepted pill shapes and their related codes.
+  # {
+  #       'BULLET'=> 'C48335',
+  #       'CAPSULE'=> 'C48336',
+  #       'CLOVER'=> 'C48337',
+  #       'DIAMOND'=> 'C48338',
+  #       'DOUBLE_CIRCLE'=> 'C48339',
+  #       'FREEFORM'=> 'C48340',
+  #       'GEAR'=> 'C48341',
+  #       'HEPTAGON'=> 'C48342',
+  #       'HEXAGON'=> 'C48343',
+  #       'OCTAGON'=> 'C48344',
+  #       'OVAL'=> 'C48345',
+  #       'PENTAGON'=> 'C48346',
+  #       'RECTANGLE'=> 'C48347',
+  #       'ROUND'=> 'C48348',
+  #       'SEMI_CIRCLE'=> 'C48349',
+  #       'SQUARE'=> 'C48350',
+  #       'TEAR'=> 'C48351',
+  #       'TRAPEZOID'=> 'C48352',
+  #       'TRIANGLE'=> 'C48353'
+  #   }
+  def self.shapes
+    SHAPES.inject({}){|i,(k,v)| i.merge k.humanize => v }
+  end
 
   COLORS = {
       'BLACK'=> 'C48323',
@@ -98,7 +129,26 @@ class PillboxResource < ActiveResource::Base
       'YELLOW'=> 'C48330'
   }
   COLOR_CODES = COLORS.invert
-  def self.colors; COLORS.inject({}){|i,(k,v)| i.merge k.humanize => v } end
+  
+  ##
+  # Returns a hash of the accepted pill colors and their related codes.
+  # {
+  #       'BLACK'=> 'C48323',
+  #       'BLUE'=> 'C48333',
+  #       'BROWN'=> 'C48332',
+  #       'GRAY'=> 'C48324',
+  #       'GREEN'=> 'C48329',
+  #       'ORANGE'=> 'C48331',
+  #       'PINK'=> 'C48328',
+  #       'PURPLE'=> 'C48327',
+  #       'RED'=> 'C48326',
+  #       'TURQUOISE'=> 'C48334',
+  #       'WHITE'=> 'C48325',
+  #       'YELLOW'=> 'C48330'
+  #   }
+  def self.colors
+    COLORS.inject({}){|i,(k,v)| i.merge k.humanize => v }
+  end
   
   DEA_CODES = {
       'I'   => 'C48672',
@@ -108,19 +158,54 @@ class PillboxResource < ActiveResource::Base
       'V'   => 'C48679'
   }
   SCHEDULE_CODES = DEA_CODES.invert
-  def self.dea_codes; DEA_CODES.inject({}) { |i,(k,v)| i.merge k.humanize => v } end
+  
+  ##
+  # Returns a hash of the accepted DEA Schedule codes and their related API codes
+  # {
+  #     'I'   => 'C48672',
+  #     'II'  => 'C48675',
+  #     'III' => 'C48676',
+  #     'IV'  => 'C48677',
+  #     'V'   => 'C48679'
+  # }
+  def self.dea_codes
+    DEA_CODES.inject({}) { |i,(k,v)| i.merge k.humanize => v }
+  end
 
   cattr_accessor :api_key
-  attr_accessor :color2
   
+  ##
+  # Returns as an integer the number of records returned from the previous API call.
   def self.record_count
     @record_count.to_i
   end
   
+  ##
+  # This class method sets the api_key parameter to the default key listed in the yaml
+  # file under fixtures/test_api_key.yml.  This is useful for putting a call to
+  # PillboxResource.test! in your setup method in your test suite
   def self.test!
     "Using testing api_key" if self.api_key = load_test_key
   end
 
+  ##
+  # Accepts a first argument of :first or :all to narrow search results.  Accepts a params 
+  # hash as the second argument consistent with ActiveResource.find.
+  #
+  # Accepted parameters are:
+  #   'color'       => 'string' or Array with multiple colors (see http://pillbox.nlm.nih.gov/API-documentation.html)
+  #   'score'       =>  integer
+  #   'ingredient'  => 'string' or Array with multiple ingredients (returned results include all ingredients) 
+  #   'inactive'    => 'string' 
+  #   'dea'         => 'string' or any of 'I, II, III, IV, V'
+  #   'author'      => 'string'
+  #   'shape'       => 'string' (Shape or Hex)
+  #   'imprint'     => 'string'
+  #   'prodcode'    => 'string' (Product Code: see http://pillbox.nlm.nih.gov/API-documentation.html)
+  #   'has_image'   => 0 or 1 for no image present or image present respectively
+  #   'size'        => integer for size in millimeters (currently this encompasses a range of +/- 2 mm)
+  #   'lower_limit' => integer for which returned record to start at (currently non-functional)
+  # 
   def self.find(first, options={})
     # MYTODO :| ok for now... but this only works with rails
     options = HashWithIndifferentAccess.new(options)
@@ -136,12 +221,15 @@ class PillboxResource < ActiveResource::Base
       end
     end
   end
-  
-  def respond_to?(meth)
+
+  ##
+  # These two metaprogramming methods implemented to give flexibility when querying a PillboxResource object
+  # for its information.
+  def respond_to?(meth) # :nodoc:
     (attributes.has_key?(meth.to_s.upcase) || attributes.has_key?(meth.to_s)) ? true : super
   end
   
-  def method_missing(method, *args, &block)
+  def method_missing(method, *args, &block) # :nodoc:
     if attributes.has_key?(method.to_s.upcase)
       attributes[method.to_s.upcase].nil? ? [] : attributes[method.to_s.upcase]
     elsif attributes.has_key?(method.to_s)
@@ -151,13 +239,17 @@ class PillboxResource < ActiveResource::Base
     end
   end
 
-  def shape # handle multi-color (OUTPUT ONLY)
+  ##
+  # Returns the pill shape as a human readable shape description or as a 'shape code'.
+  def shape # handle multi-color
     return nil unless attributes['SPLSHAPE']
     attributes['SPLSHAPE'].split(";").map do |shape_code|
       SHAPE_CODES[shape_code] || shape_code
     end
   end
   
+  ##
+  # Returns the pill color as a human readable color description or as a 'color code'.
   def color
     return nil unless attributes['SPLCOLOR']
     attributes['SPLCOLOR'].split(";").map do |color_code|
@@ -165,15 +257,45 @@ class PillboxResource < ActiveResource::Base
     end
   end
 
+  ##
+  # Returns the product code as a string, 9-digit FDA code listed at:
+  # http://www.fda.gov/Drugs/InformationOnDrugs/ucm142438.htm
   def prodcode; attributes['PRODUCT_CODE'] end
+  
+  ##
+  # Returns a url as a string for looking up the drug monograph using the
+  # http://druginfo.nlm.nih.gov/drugportal API
   def info_url; "http://druginfo.nlm.nih.gov/drugportal/dpdirect.jsp?name="+ingredient end
+  
+  ##
+  # Returns true if the pill has an associated image, false if it does not.
   def has_image?; attributes['HAS_IMAGE'] == '1' end
+  
+  ##
+  # Returns a string list of ingredients. Does not contain brand/trade names.
   def ingredient; self.ingredients end
+  
+  ##
+  # Returns the size of the pill in millimeters as a float. Values contain two decimal places.
   def size; attributes['SPLSIZE'].to_f end
+  
+  ##
+  # Returns an integer representing the score value see http://goo.gl/SzCO for details.
   def score; attributes['SPLSCORE'] end
+  
+  ##
+  # Returns the string representation of any alphanumeric text appearing on the pill.
   def imprint; attributes['splimprint'] end
-  def trade_name; self.rxstring.split(" ").first.downcase end
+  
+  ##
+  # Returns the trade brand/trade name as a capitalized string.  This is not the generic name of the active ingredient.
+  # For example the trade name of 'sildenafil' would be 'Viagra'.
+  def trade_name; self.rxstring.split(" ").first.downcase.capitalize end
 
+  ##
+  # Accepts image_size argument as one of super_small, small, medium, large, or all.  Default is super_small.
+  # Returns the url, as a string, of the corresponding image when all is not given as the image size argument.
+  # Returns an array of urls representing all sizes of the pill image when 'all' is given as an argument
   def image_url(image_size = 'super_small')
     unless image_id 
       return nil
@@ -183,43 +305,56 @@ class PillboxResource < ActiveResource::Base
       when "small";       "http://pillbox.nlm.nih.gov/assets/small/#{image_id}sm.jpg" 
       when "medium";      "http://pillbox.nlm.nih.gov/assets/medium/#{image_id}md.jpg"
       when "large";       "http://pillbox.nlm.nih.gov/assets/large/#{image_id}lg.jpg"
+      when "all"
+        ["http://pillbox.nlm.nih.gov/assets/super_small/#{image_id}ss.png",
+         "http://pillbox.nlm.nih.gov/assets/small/#{image_id}sm.jpg",
+         "http://pillbox.nlm.nih.gov/assets/medium/#{image_id}md.jpg",
+         "http://pillbox.nlm.nih.gov/assets/large/#{image_id}lg.jpg"]
     end
   end
   
+  ##
+  # Returns a string with the DEA schedule code with 'Schedule' prepended, i.e...
+  #   pill.dea  => 'Schedule II'
   def dea
     return nil unless attributes['DEA_SCHEDULE_CODE']
     "Schedule #{SCHEDULE_CODES[attributes['DEA_SCHEDULE_CODE'].to_s]}" 
   end
   
+  ##
+  # Returns the inactive ingredients as an array with double quotes removed.  In the current API
+  # double quotes are used instead of spaces between ingredients.
   def inactive
     # Remove double quotes from the inactive ingredients list
     Array(attributes['SPL_INACTIVE_ING'].split("/")).map { |str| str.gsub('"', ' ').strip}
   end
   
+  ##
+  # Returns a string representing the author of the drug monograph with spaces and double quotes removed.
   def author
     # Remove double quotes from the author attribute
     attributes['AUTHOR'].gsub('"', ' ').strip
   end
 
 private
-  def self.load_test_key
+  def self.load_test_key # :nodoc:
     test_key = YAML.load_file("#{File.expand_path(File.dirname(__FILE__) + '/../test/fixtures/test_api_key.yml')}")
     test_key[:key]
   end
   
-  VALID_ATTRIBUTE_NAMES = %w(color score ingredient ingredients inactive dea author shape imprint prodcode has_image size lower_limit product_code)
+  VALID_ATTRIBUTE_NAMES = %w(color score ingredient ingredients inactive dea author shape imprint prodcode has_image size lower_limit)
 
-  def self.validate_pillbox_api_params(options)
+  def self.validate_pillbox_api_params(options) # :nodoc:
     validate_presence_of_api_key(options)
     raise "try using find :all, :params => { ... }  with one of these options: #{VALID_ATTRIBUTE_NAMES.inspect}" unless options[:params].is_a?(Hash)
     raise "valid params options are:  #{VALID_ATTRIBUTE_NAMES.inspect}  ... you have invalid params option(s): #{(VALID_ATTRIBUTE_NAMES && options[:params].keys) - VALID_ATTRIBUTE_NAMES}" unless ((VALID_ATTRIBUTE_NAMES && options[:params].keys) - VALID_ATTRIBUTE_NAMES).empty?
   end
   
-  def self.validate_presence_of_api_key(options)
+  def self.validate_presence_of_api_key(options) # :nodoc:
     raise "You must define api key. PillboxResource.api_key = 'YOUR SECRET KEY'" unless (self.api_key or options[:params][:key])
   end
   
-  def self.interpret_params(options = {})
+  def self.interpret_params(options = {}) # :nodoc:
     # puts options
     @params = HashWithIndifferentAccess.new(options['params']) || {}
     @params['key'] ||= self.api_key
@@ -241,7 +376,7 @@ private
     return options
   end
   
-  def self.parse_color
+  def self.parse_color # :nodoc:
     begin
       @params['color'] = case @params['color']
       when NilClass; 
@@ -254,7 +389,7 @@ private
     end
   end
   
-  def self.parse_shape
+  def self.parse_shape # :nodoc:
     begin
       @params['shape'] = case @params['shape']
       when NilClass; 
@@ -268,20 +403,20 @@ private
     end
   end
   
-  def self.parse_prodcode
+  def self.parse_prodcode # :nodoc:
     # todo: prodcode
     begin
-      @params['product_code'] = case @params['product_code']
+      @params['prodcode'] = case @params['prodcode']
       # when nil;                puts "i can see my house!  "#raise "Product code cannot be nil" # Schema says PRODUCT_CODE cannot be NULL
-      # when Array;                   params['product_code'].join(";")
-      when /\A(\d{3,}-\d{3,4})\z/;  @params['product_code']
+      # when Array;                   params['prodcode'].join(";")
+      when /\A(\d{3,}-\d{3,4})\z/;  @params['prodcode']
       else;
       end
     rescue
     end
   end
   
-  def self.parse_dea
+  def self.parse_dea # :nodoc:
     begin
       @params['dea'] = case @params['dea']
       when NilClass;
@@ -295,7 +430,7 @@ private
     end
   end
   
-  def self.parse_ingredient
+  def self.parse_ingredient # :nodoc:
     begin
       @params['ingredient'] = case @params['ingredient']
       when NilClass;
--- test/pillbox_resource_test.rb
+++ test/pillbox_resource_test.rb
@@ -9,55 +9,54 @@ class PillboxResourceTest < Test::Unit::TestCase
   end
   
   def test_should_find_by_combo_shape
-    med = PillboxResource.find(:first, :params=>{'color'=>"C48324;C48323"})
+    med = PillboxResource.first(:params=>{'color'=>"C48324;C48323"})
     assert_equal("Acetaminophen 150 MG / Aspirin 180 MG / Codeine 30 MG Oral Capsule", med.rxstring)
   end
   
   def test_should_accept_shape_as_hex
-    meds = PillboxResource.find(:all, :params=>{'shape' => 'C48336'})
-    # puts PillboxResource.instance_eval { puts params }
+    meds = PillboxResource.all(:params=>{'shape' => 'C48336'})
     assert_equal(2345, PillboxResource.record_count)
   end
   
   def test_should_accept_color_as_hex
-    meds = PillboxResource.find(:all, :params => {'color' => "C48328"})
+    meds = PillboxResource.all(:params => {'color' => "C48328"})
     assert_equal(886, PillboxResource.record_count)
   end
   
   def test_should_accept_multiple_colors
-    meds = PillboxResource.find(:all, :params => { 'color' => ['C48328', 'C48327']})
+    meds = PillboxResource.all(:params => { 'color' => ['C48328', 'C48327']})
     assert_equal(1, meds.count)
     assert_equal('Selfemra 20 MG Oral Capsule', meds.first.rxstring)
   end
   
   def test_should_accept_shape_as_string
-    meds = PillboxResource.find(:all, :params => {'shape' => 'capsule'})
+    meds = PillboxResource.all(:params => {'shape' => 'capsule'})
     assert_equal(2345, PillboxResource.record_count)
     meds.each { |m| assert_equal('capsule', m.shape.kind_of?(Array) ? m.shape.first.downcase : m.shape.downcase) }
   end
   
   def test_should_accept_color_as_string
-    meds = PillboxResource.find(:all, :params => {'color' => 'white'})
+    meds = PillboxResource.all(:params => {'color' => 'white'})
     assert_equal(4364, PillboxResource.record_count)
   end
   
   def test_param_combinations_should_work
-    meds = PillboxResource.find(:all, :params => {'shape' => 'C48348', 'color' => 'C48325'})
+    meds = PillboxResource.all(:params => {'shape' => 'C48348', 'color' => 'C48325'})
     assert_equal(2401, PillboxResource.record_count)
   end
   
   def test_imprint_search_should_work
-    meds = PillboxResource.find(:all, :params => {'imprint' => 'NVR'})
+    meds = PillboxResource.all(:params => {'imprint' => 'NVR'})
     assert_equal(69, PillboxResource.record_count)
   end
   
   def test_size_search_should_work
-    meds = PillboxResource.find(:all, :params => {'size' => '12.00'})
+    meds = PillboxResource.all(:params => {'size' => '12.00'})
     assert_equal(3082, PillboxResource.record_count)
   end
   
   def test_should_return_normalized_array_of_strings_for_inactive_ingredients
-    meds = PillboxResource.find(:all, :params => {:has_image => true})
+    meds = PillboxResource.all(:params => {:has_image => true})
     assert_equal(Array, meds.first.inactive.class)
     meds.first.inactive.each do |ing|
       assert_no_match(/\s$/, ing)
@@ -67,24 +66,39 @@ class PillboxResourceTest < Test::Unit::TestCase
   end
   
   def test_should_return_appropriate_dea_schedule
-    med = PillboxResource.find(:first, :params => {:ingredient => 'Hydromorphone'})
+    med = PillboxResource.first(:params => {:ingredient => 'Hydromorphone'})
     assert_equal('Schedule II', med.dea)
   end
   
   def test_should_rescue_rexml_parser_error
-    assert_nothing_raised { med = PillboxResource.find(:all, :params => {:dea => "a"} ) }
+    assert_nothing_raised { med = PillboxResource.all(:params => {:dea => "a"} ) }
   end
   
   def test_should_allow_searching_for_multiple_active_ingredients
-    med = PillboxResource.find(:all, :params => {:ingredient => ['amlodipine', 'benazepril']})
+    med = PillboxResource.all(:params => {:ingredient => ['amlodipine', 'benazepril']})
     assert_equal(28, med.count)
-    med = PillboxResource.find(:all, :params => {:ingredient => ['valsartan','hydrochlorothiazide', 'amlodipine']})
+    med = PillboxResource.all(:params => {:ingredient => ['valsartan','hydrochlorothiazide', 'amlodipine']})
     assert_not_nil(med)
     assert_equal(5, med.count)
   end
   
-  def should_return_a_valid_image_url
-    meds = PillboxResource.find(:all, :params => {:has_image => 1})
+  def test_should_return_a_valid_image_url
+    meds = PillboxResource.all(:params => {:has_image => 1})
     meds.each { |m| assert_equal("http://pillbox.nlm.nih.gov/assets/super_small/#{m.image_id}ss.png", m.image_url)}
+  end
+  
+  def test_should_return_an_array_of_valid_urls
+    meds = PillboxResource.all(:params => {:has_image => 1})
+    meds.each do |m| 
+      assert_equal(["http://pillbox.nlm.nih.gov/assets/super_small/#{m.image_id}ss.png",
+                                  "http://pillbox.nlm.nih.gov/assets/small/#{m.image_id}sm.jpg",
+                                  "http://pillbox.nlm.nih.gov/assets/medium/#{m.image_id}md.jpg",
+                                  "http://pillbox.nlm.nih.gov/assets/large/#{m.image_id}lg.jpg"], m.image_url('all'))
+    end
+  end
+  
+  def test_should_return_a_valid_trade_name
+    med = PillboxResource.first(:params => {:ingredient  => 'Sildenafil'})
+    assert_equal('Viagra', med.trade_name)
   end
 end
